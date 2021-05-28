11800677dc2c602227203b432f0becc37506c9dd
matute <matute@debian>
2021-05-27 23:43:40 -0300

    Update

--- elisp/gui2.el
+++ elisp/gui2.el
@@ -40,10 +40,10 @@
    :config 
    (setq  centaur-tabs-set-bar 'under
 	   x-underline-at-descent-line t
-          centaur-tabs-style "slant"
+          centaur-tabs-style "bar"
           centaur-tabs-set-icons t
           centaur-tabs-gray-out-icons 'buffer
-          centaur-tabs-height 40
+          centaur-tabs-height 35
           centaur-tabs-set-modified-marker t
           centaur-tabs-modifier-marker "*")
        (centaur-tabs-mode t))
