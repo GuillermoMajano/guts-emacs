37c367ac537aa8817b1c454b72f877f663e8ad99
Matute <7433304-matute_1@users.noreply.gitlab.com>
2021-05-31 23:00:23 -0300

    UPDATE

--- init.el
+++ init.el
@@ -1,2 +1,16 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elcord-editor-icon "emacs_material_icon")
+ '(package-selected-packages
+   '(doom-themes use-package treemacs-projectile treemacs-all-the-icons kaolin-themes flycheck dashboard counsel company centaur-tabs)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
